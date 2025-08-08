78454bad87a7b1002e4def0e2dba380ba28ae01d
DIPAKK2310 <dipakkhare217@gmail.com>
2025-08-08 14:01:13 +0530

    Initial commit

---
+++ client/src/App.tsx
@@ -0,0 +1,13 @@
+import "./index.css"
+
+const App = () => {
+  return (
+    <div className="h-screen flex items-center justify-center bg-gray-100">
+    <h1 className="text-4xl font-bold text-blue-600">
+      AI Interview Platform (Vapi + Gemini)
+    </h1>
+  </div>
+  )
+}
+
+export default App
